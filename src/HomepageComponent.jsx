586d25473bf48e0ab5e6231b2767929bdbdd19d7
Victor Agbeo <75548415+saintvic@users.noreply.github.com>
2021-11-13 13:12:54 -0600

    Created files n added sass for homepage

---
+++ src/HomepageComponent.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import './homepage.styles.scss';
+
+// our homepage simplified without any styling.
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      {/* each card on the homepage */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title"> HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title"> JACKETS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title"> WOMENS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title"> MENS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
